21576d4f8c840ddb0b6586ff1a1cbcf83d6926f0
Gleyson Sampaio <izapay4devs@gmail.com>
2024-09-30 19:21:41 -0300

    Initial commit

---
+++ src/main/java/com/aml/auth/MsAuthApplication.java
@@ -0,0 +1,13 @@
+package com.aml.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAuthApplication.class, args);
+	}
+
+}
